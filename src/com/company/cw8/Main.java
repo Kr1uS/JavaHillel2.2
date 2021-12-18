c735666b60702bb2313ebf2bdb31f3bd234183b7
natalinateli <ntbiliei@gmail.com>
2021-12-18 13:23:53 +0200

    class work 8 exceptions

--- src/com/company/cw8/Main.java
+++ src/com/company/cw8/Main.java
@@ -2,8 +2,21 @@ package com.company.cw8;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
 
+        test();
+
+    }
+
+    private static String test() throws MyException {
+
+        try {
+            System.out.println("try");
+        } catch (NullPointerException exception) {
+            throw new MyException("MyException was caught", exception);
+        }
+
+        return "";
     }
 
 }
