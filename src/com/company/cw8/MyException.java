8e939451764ce721ddf76af5d03c49977e12862b
Kirill <72558733+Kr1uS@users.noreply.github.com>
2021-12-25 14:22:38 +0200

    Delete src/com/company/cw8 directory

--- src/com/company/cw8/MyException.java
+++
@@ -1,9 +0,0 @@
-package com.company.cw8;
-
-public class MyException extends Exception {
-
-    public MyException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-
-}
